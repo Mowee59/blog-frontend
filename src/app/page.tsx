148933e8d2eddd3de364501770e4ae0881897c14
Aniss <mahfoudi.apb@live.fr>
2024-05-22 16:18:03 +0200

    Started ArticleCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import ArticleCard from "@/components/ui/article-card/ArticleCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-6 lg:max-w-screen-lg">
-      <h1>Home page</h1>
+    <main className="container mx-auto px-6 sm:px-0 lg:max-w-screen-lg">
+      <ArticleCard />
     </main>
   );
 }
---
+++ src/components/ui/article-card/ArticleCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ArticleCard = () => {
+  return (
+    <article className="@container">
+      <div>
+        <h3>2 Février</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Ac porta gravida sed integer
+          fusce commodo gravida tellus.
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default ArticleCard;
