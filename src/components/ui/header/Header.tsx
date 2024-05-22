c6322dd163d2fb896ce6b3dbe23af9e1ea667d46
Aniss <mahfoudi.apb@live.fr>
2024-05-22 14:38:54 +0200

    fixed header height

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -4,8 +4,8 @@ import ThemeSwitch from "./ThemeSwitch";
 
 const Header = () => {
   return (
-    <header className="mb-20 h-9 px-6 pt-5 sm:pt-10 ">
-      <div className="container mx-auto flex items-center justify-between lg:max-w-screen-lg">
+    <header className="mb-20  px-6 pt-5 sm:pt-10">
+      <div className="container mx-auto flex h-9 items-center justify-between lg:max-w-screen-lg">
         <h4 className="text-base font-medium leading-tight text-neutral-900 sm:text-xl dark:text-neutral-300">
           Blog
         </h4>
