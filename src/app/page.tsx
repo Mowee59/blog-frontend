cd88966a3543589d8cf02f8ea2a058938571a83c
Aniss <mahfoudi.apb@live.fr>
2024-05-24 13:45:36 +0200

    Home page + correcting Article card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
 import ArticleCard from "@/components/ui/article-card/ArticleCard";
 import Image from "next/image";
+import hero from "/public/svg/hero.jpg";
 
 export default function Home() {
   return (
-    <main className="container  flex flex-col gap-7 lg:max-w-screen-lg">
+    <main className="container flex flex-col gap-7 lg:max-w-screen-lg">
+      <div className=" lg:px-10 xl:px-14">
+        <h1 className="mb-6 text-2xl font-medium leading-normal text-neutral-900 dark:text-neutral-300 sm:text-4xl sm:leading-10 lg:mb-8 lg:text-5xl xl:text-6xl">
+          Lorem ipsum dolor sit amet consectetur.
+        </h1>
+        <p className="text-base font-medium text-neutral-700 dark:text-neutral-400 sm:text-lg sm:leading-7 lg:text-xl lg:leading-loose">
+          Lorem ipsum dolor sit amet consectetur. Lectus fringilla amet
+          fringilla maecenas. Volutpat in volutpat dui placerat nec in. Volutpat
+          id.
+        </p>
+      </div>
+      {/* #TODO Handle image sizing */}
+      <div>
+        <div className="relative mx-auto  mb-4 h-[340px] w-full lg:h-[500px] ">
+          <Image
+            src={hero}
+            alt="hero image"
+            fill
+            className=" rounded-xl object-cover"
+          />
+        </div>
+        <p className="text-center text-xs font-normal italic leading-none text-neutral-600 dark:text-neutral-500">
+          Exploring the mountains in Indonesia - Unsplash
+        </p>
+      </div>
       <ArticleCard />
       <ArticleCard />
       <ArticleCard />
--- src/components/ui/article-card/ArticleCard.tsx
+++ src/components/ui/article-card/ArticleCard.tsx
@@ -10,7 +10,7 @@ const ArticleCard = () => {
           <h3 className=" mb-3 text-xs font-medium leading-3 text-neutral-600 @lg:mb-4 dark:text-neutral-400">
             2 Fév - #tag
           </h3>
-          <p className="text-sm font-medium leading-tight text-neutral-800 dark:text-neutral-300">
+          <p className="text-sm font-medium leading-tight text-neutral-800 dark:text-neutral-300 sm:text-base sm:leading-normal  lg:text-lg lg:leading-relaxed">
             Lorem ipsum dolor sit amet consectetur. Ac porta gravida sed integer
             fusce commodo gravida tellus.
           </p>
