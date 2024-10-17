8b8e05f377d8dc4988129bd866af3a42c90b9ebb
Aniss <mahfoudi.apb@live.fr>
2024-10-17 13:43:32 +0200

    Refactor: Integrate Google Analytics as a Component
    - Removed inline Google Analytics script tags from `layout.tsx`.
    - Added a new `GoogleAnalytics` component for better modularity and maintainability.
    - Ensured the `GoogleAnalytics` component is loaded within the `RootLayout` to maintain functionality.
    - This change improves code organization and adheres to best practices for component-based architecture.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-import Script from 'next/script';import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/ui/header/Header";
 import Footer from "@/components/ui/footer/Footer";
 import { ThemeProviderComponent } from "@/providers/ThemeProviderComponent";
 import "./globals.css";
 import ReactQueryProviderComponent from "@/providers/ReactQueryProviderComponent";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,24 +27,8 @@ export default function RootLayout({
   return (
     // Warning: Hydration failed because the initial UI does not match what was returned from the server, because of the theme handling
     <html lang="fr" suppressHydrationWarning>
-            {/* Google Analytics */}
-            <Script async
-        id="google-analytics"
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-T1XTL8NY99`}
-      />
-      <Script id="ga-setup" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-T1XTL8NY99', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-      
       <body className={inter.className}>
+        <GoogleAnalytics />
         <ThemeProviderComponent>
           <ReactQueryProviderComponent>
             <Header />
